2811e425a83db12706d4a2bb3342c9e40d4d7d48
Lindoélio Lázaro <lindoelio@gmail.com>
2017-08-08 22:49:16 -0300

    Improving linter...

--- imports/api/links/server/publications.tests.js
+++ imports/api/links/server/publications.tests.js
@@ -2,27 +2,27 @@
 //
 // https://guide.meteor.com/testing.html
 
-import { assert } from 'meteor/practicalmeteor:chai';
-import { Links } from '../links.js';
-import { PublicationCollector } from 'meteor/johanbrook:publication-collector';
-import './publications.js';
+import { assert } from 'meteor/practicalmeteor:chai'
+import { Links } from '../links.js'
+import { PublicationCollector } from 'meteor/johanbrook:publication-collector'
+import './publications.js'
 
 describe('links publications', function () {
   beforeEach(function () {
-    Links.remove({});
+    Links.remove({})
     Links.insert({
       title: 'meteor homepage',
-      url: 'https://www.meteor.com',
-    });
-  });
+      url: 'https://www.meteor.com'
+    })
+  })
 
   describe('links.all', function () {
     it('sends all links', function (done) {
-      const collector = new PublicationCollector();
+      const collector = new PublicationCollector()
       collector.collect('links.all', (collections) => {
-        assert.equal(collections.links.length, 1);
-        done();
-      });
-    });
-  });
-});
+        assert.equal(collections.links.length, 1)
+        done()
+      })
+    })
+  })
+})
