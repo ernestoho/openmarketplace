3fa9a2c5b43afec94ec9c81f2c3705f753487db7
Lindoélio Lázaro <lindoelio@gmail.com>
2017-09-16 22:31:49 -0300

    [WIP] Adding Apollo GraphQL

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ import '/imports/startup/both'
 import { createApolloServer } from 'meteor/apollo'
 import { makeExecutableSchema } from 'graphql-tools'
 
-import { typeDefs, resolvers } from '/imports/api/schema'
+import { typeDefs, resolvers } from '/imports/api/schema.js'
 
 const schema = makeExecutableSchema({
   typeDefs,
